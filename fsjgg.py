7f913402e53965959ddc3d68a7e0a7ed9de2014d
Unknown <allium.fistulosum.39@gmail.com>
2019-03-14 05:25:07 +0900

    add choose_pop_***

--- fsjgg.py
+++ fsjgg.py
@@ -32,22 +32,68 @@ class FSJGG(object):
 	def get_best_fitness(self):
 		return min(self.fs.get_best_fitness(), self.jgg.get_best_fitness())
 
+	def choose_population_to_jgg_not_replace(self):
+		return self.init_population[:]
+
 	def choose_population_to_jgg_replace_rand_parents_by_elites(self, elites_count = None):
 		if elites_count is None:
 			elites_count = self.npar
-		ret = self.init_population[:]
-		np.random.shuffle(ret)
-		ret = ret[elites_count:]
+		pop = self.init_population[:]
+		pop.sort(key = lambda i: i.fitness)
+		not_parents = pop[self.npar:]
+		ret = not_parents[:]
+		parents = pop[:self.npar]
+		np.random.shuffle(parents)
+		ret.extend(parents[:self.npar - elites_count])
 		elites = self.fs.population[:]
 		elites.sort(key = lambda i: i.fitness)
 		ret.extend(elites[:elites_count])
 		return ret
 
+	def choose_population_to_jgg_replace_losed_parents_by_elites(self, elites_count = None):
+		if elites_count is None:
+			elites_count = self.npar
+		pop = self.init_population[:]
+		pop.sort(key = lambda i: i.fitness)
+		not_parents = pop[self.npar:]
+		ret = not_parents[:]
+		parents = pop[:self.npar]
+		ret.extend(parents[:self.npar - elites_count])
+		elites = self.fs.population[:]
+		elites.sort(key = lambda i: i.fitness)
+		ret.extend(elites[:elites_count])
+		return ret
+
+	def choose_population_to_jgg_replace_rand_by_elites(self, elites_count = None):
+		if elites_count is None:
+			elites_count = self.npar
+		pop = self.init_population[:]
+		np.random.shuffle(pop)
+		ret = pop[elites_count:]
+		elites = self.fs.population[:]
+		elites.sort(key = lambda i: i.fitness)
+		ret.extend(elites[:elites_count])
+		print(len(ret))
+		return ret
+
+	def choose_population_to_jgg_replace_losed_by_elites(self, elites_count = None):
+		if elites_count is None:
+			elites_count = self.npar
+		pop = self.init_population[:]
+		pop.sort(key = lambda i: -i.fitness)
+		ret = pop[elites_count:]
+		elites = self.fs.population[:]
+		elites.sort(key = lambda i: i.fitness)
+		ret.extend(elites[:elites_count])
+		print(len(ret))
+		return ret
+
 if __name__ == '__main__':
 	n = 20
-	ga = FSJGG(1e-4, n, 6 * n, n + 1, 6 * n, lambda x: np.sum((x * 10.24 - 5.12) ** 2))
+	ga = FSJGG(1e-2, n, 6 * n, n + 1, 6 * n, lambda x: np.sum((x * 10.24 - 5.12) ** 2))
 
-	ga.choose_population_to_jgg = ga.choose_population_to_jgg_replace_rand_parents_by_elites
+	ga.choose_population_to_jgg = lambda :\
+		ga.choose_population_to_jgg_replace_rand_parents_by_elites(7)
 	ga.until(1e-7, 300000)
 	print(ga.get_best_fitness(), ga.get_eval_count())
 
