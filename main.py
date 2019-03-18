05155cc21355a0ca223bbc40cb19658494b67517
Unknown <allium.fistulosum.39@gmail.com>
2019-03-19 03:26:12 +0900

    more pop, more diversity

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ n = 20
 
 problems = [
 	# {"name" : "sphere",      "func" : sphere,      "npop" :  6 * n, "nchi" : 6 * n},
-	# {"name" : "k-tablet",    "func" : ktablet,     "npop" :  8 * n, "nchi" : 6 * n},
-	# {"name" : "bohachevsky", "func" : bohachevsky, "npop" :  6 * n, "nchi" : 6 * n},
-	{"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
-	# {"name" : "schaffer",    "func" : schaffer,    "npop" : 10 * n, "nchi" : 8 * n},
+	# {"name" : "k-tablet",    "func" : ktablet,     "npop" :  9 * n, "nchi" : 6 * n},
+	# {"name" : "bohachevsky", "func" : bohachevsky, "npop" :  8 * n, "nchi" : 6 * n},
+	# {"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
+	{"name" : "schaffer",    "func" : schaffer,    "npop" : 11 * n, "nchi" : 8 * n},
 	# {"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
 ]
 
@@ -64,26 +64,6 @@ for problem in problems:
 		np.random.seed(randseed)
 		fsjgg = FSJGG(t, n, npop, npar, nchi, func)
 		fsjgg.choose_population_to_jgg =\
-			fsjgg.choose_population_to_jgg_not_replace
-		result = fsjgg.until(1e-7, 300000)
-		if result:
-			if "入替無" in eval_counts:
-				eval_counts["入替無"].append(fsjgg.get_eval_count())
-			else:
-				eval_counts["入替無"] = [fsjgg.get_eval_count()]
-		else:
-			print("入替無 failed", randseed)
-
-		if SAVE_CSV:
-			filename = "benchmark/{0}_入替無_{1}_{2}.csv".format(datestr, name, i)
-			with open(filename, "w") as f:
-				for c, v in fsjgg.history.items():
-					f.write("{0},{1}\n".format(c, v))
-				f.close()
-
-		np.random.seed(randseed)
-		fsjgg = FSJGG(t, n, npop, npar, nchi, func)
-		fsjgg.choose_population_to_jgg =\
 			fsjgg.choose_population_to_jgg_replace_rand_parents_by_elites
 		result = fsjgg.until(1e-7, 300000)
 		if result:
@@ -100,86 +80,6 @@ for problem in problems:
 				for c, v in fsjgg.history.items():
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
-
-		# np.random.seed(randseed)
-		# fsjgg = FSJGG(t, n, npop, npar, nchi, func)
-		# fsjgg.choose_population_to_jgg = lambda :\
-		# 	fsjgg.choose_population_to_jgg_replace_rand_parents_by_elites(npar // 3)
-		# result = fsjgg.until(1e-7, 300000)
-		# if result:
-		# 	if "ラ親" in eval_counts:
-		# 		eval_counts["ラ親"].append(fsjgg.get_eval_count())
-		# 	else:
-		# 		eval_counts["ラ親"] = [fsjgg.get_eval_count()]
-		# else:
-		# 	print("ラ親 failed", randseed)
-
-		# if SAVE_CSV:
-		# 	filename = "benchmark/{0}_ラ親_{1}_{2}.csv".format(datestr, name, i)
-		# 	with open(filename, "w") as f:
-		# 		for c, v in fsjgg.history.items():
-		# 			f.write("{0},{1}\n".format(c, v))
-		# 		f.close()
-
-		# np.random.seed(randseed)
-		# fsjgg = FSJGG(t, n, npop, npar, nchi, func)
-		# fsjgg.choose_population_to_jgg = lambda :\
-		# 	fsjgg.choose_population_to_jgg_replace_losed_parents_by_elites(npar // 3)
-		# result = fsjgg.until(1e-7, 300000)
-		# if result:
-		# 	if "劣親" in eval_counts:
-		# 		eval_counts["劣親"].append(fsjgg.get_eval_count())
-		# 	else:
-		# 		eval_counts["劣親"] = [fsjgg.get_eval_count()]
-		# else:
-		# 	print("劣親 failed", randseed)
-
-		# if SAVE_CSV:
-		# 	filename = "benchmark/{0}_劣親_{1}_{2}.csv".format(datestr, name, i)
-		# 	with open(filename, "w") as f:
-		# 		for c, v in fsjgg.history.items():
-		# 			f.write("{0},{1}\n".format(c, v))
-		# 		f.close()
-
-		# np.random.seed(randseed)
-		# fsjgg = FSJGG(t, n, npop, npar, nchi, func)
-		# fsjgg.choose_population_to_jgg =\
-		# 	fsjgg.choose_population_to_jgg_replace_rand_by_elites
-		# result = fsjgg.until(1e-7, 300000)
-		# if result:
-		# 	if "ラ" in eval_counts:
-		# 		eval_counts["ラ"].append(fsjgg.get_eval_count())
-		# 	else:
-		# 		eval_counts["ラ"] = [fsjgg.get_eval_count()]
-		# else:
-		# 	print("ラ failed", randseed)
-
-		# if SAVE_CSV:
-		# 	filename = "benchmark/{0}_ラ_{1}_{2}.csv".format(datestr, name, i)
-		# 	with open(filename, "w") as f:
-		# 		for c, v in fsjgg.history.items():
-		# 			f.write("{0},{1}\n".format(c, v))
-		# 		f.close()
-
-		# np.random.seed(randseed)
-		# fsjgg = FSJGG(t, n, npop, npar, nchi, func)
-		# fsjgg.choose_population_to_jgg =\
-		# 	fsjgg.choose_population_to_jgg_replace_losed_by_elites
-		# result = fsjgg.until(1e-7, 300000)
-		# if result:
-		# 	if "劣" in eval_counts:
-		# 		eval_counts["劣"].append(fsjgg.get_eval_count())
-		# 	else:
-		# 		eval_counts["劣"] = [fsjgg.get_eval_count()]
-		# else:
-		# 	print("劣 failed", randseed)
-
-		# if SAVE_CSV:
-		# 	filename = "benchmark/{0}_劣_{1}_{2}.csv".format(datestr, name, i)
-		# 	with open(filename, "w") as f:
-		# 		for c, v in fsjgg.history.items():
-		# 			f.write("{0},{1}\n".format(c, v))
-		#		f.close()
 
 	for name, eval_count in eval_counts.items():
 		print(name, np.average(eval_count), loop_count - len(eval_count))
