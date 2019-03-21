cc4fe2158cb4e0e2104a64059ec986326556c80a
Unknown <allium.fistulosum.39@gmail.com>
2019-03-22 04:19:25 +0900

    round robin ttest

--- ttest.py
+++ ttest.py
@@ -3,27 +3,27 @@ import csv
 from scipy import stats
 
 def ttest(filenames, log_scaled = False):
-	if len(filenames) != 2:
-		print("give 2 files")
+	if isinstance(filenames, str):
+		print("give 2 or more files")
 		exit(-1)
 
 	datas = {}
 
 	for filename in filenames:
-		print(filename, end = " ")
 		with open(filename, "r") as f:
 			x = []
 			y = []
 			reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)
 			datas[filename] = [x for x in reader]
-	print()
 
-	result = stats.ttest_ind(datas[filenames[0]], datas[filenames[1]])
-	print(result)
-	if result.pvalue < 0.025:
-		print("有意差あり")
-	else:
-		print("有意差なし")
+	for i in range(len(filenames)):
+		for j in range(len(filenames)):
+			result = stats.ttest_ind(datas[filenames[i]], datas[filenames[j]])
+			filename_i = filenames[i].split("\\")[-1].split(".")[0]
+			filename_j = filenames[j].split("\\")[-1].split(".")[0]
+			print(filename_i, filename_j,
+				"有意差:", "あり" if result.pvalue < 0.05 else "なし",
+				"t: {0:.3f} p: {1:.3f}".format(result.statistic[0], result.pvalue[0]))
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
