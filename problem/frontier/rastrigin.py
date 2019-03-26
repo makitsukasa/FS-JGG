1701314b96c43fc48d440b43ff98b50c62d0a171
Unknown <allium.fistulosum.39@gmail.com>
2019-03-26 21:31:18 +0900

    refactor rastrigin do not need for-loop

--- problem/frontier/rastrigin.py
+++ problem/frontier/rastrigin.py
@@ -1,10 +1,5 @@
 import numpy as np
 
 def rastrigin(x):
-	# shifted = x * 10.24 - 5.12
 	shifted = x * 5.12 - 2.56
-	n = len(x)
-	ret = 10 * n
-	for i in range(n):
-		ret += np.power(shifted[i], 2) - 10 * np.cos(2 * np.pi * shifted[i])
-	return ret
+	return sum([(i**2 + 10 - 10 * np.cos(2 * np.pi * i)) for i in shifted])
